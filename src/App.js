dd2ea0d57789c9144c4c396775889caf53d92aa5
Sobokar <eugenabond@gmail.com>
2024-06-13 15:47:30 +0300

    Removed unnecessary code and files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <footer>
+        This app is created by Yevheniia Sobokar and it is open-sourced on{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GotHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
